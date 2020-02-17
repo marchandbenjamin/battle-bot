b240f7bee7a31ace5d025348eedcf6f359fb591a
Benjamin MARCHAND <bmarchand@ineat.fr>
2020-02-17 10:29:55 +0100

    Feat : Can find the enemy position

--- README.md
+++ README.md
@@ -7,6 +7,6 @@ A simple bot designed to fight an opponent.
 ### Prerequisites 
 
 Download [Battle.jar](https://rhemish-change.000webhostapp.com/samples/IneatAcademyPooX-0.0.1-SNAPSHOT-jar-with-dependencies.jar)
-A library including a battleground simulator, three ~~stupid~~ simple opponents AI.
+A library including a battleground simulator and three ~~stupid~~ simple opponents AI.
 
 Add Battle.jar to the project libraries.
--- src/test/BattleBotTest.java
+++ src/test/BattleBotTest.java
@@ -1,7 +1,22 @@
 package test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.ineat.academy.work.poox.bo.BoardElement;
+import com.ineat.academy.work.poox.bo.BoardPosition;
+import org.junit.Test;
 
-class BattleBotTest {
+import java.util.HashMap;
+import java.util.Map;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BattleBotTest {
+
+    @Test
+    public void should_confirm_enemy_position(){
+        // ARRANGE
+
+        // ACT
+
+        // ASSERT
+    }
 }
